0464d078ca1d0a581edd071c751d90bfa88e9198
Victor Odhiambo <146205165+radical-vicky@users.noreply.github.com>
2025-01-31 16:24:22 +0300

    Update action.php

--- templates/action.php
+++ templates/action.php
@@ -1,4 +1,3 @@
-// database connection
 
 <?php
 $servername = "localhost";
@@ -13,5 +12,37 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+// check data submission
+
+if  ( $_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = trim($_POST['username']);
+    $email = trim ($_POST['email']);
+    $password = ($_POST['password']);
+    $confirmPassword = ($_POST['password1']);
+
+    //check if password matches
+    if ($password !== $confirmPassword){
+        die("The password doesn't match."); 
+}
+// hashing password for security
+
+$hashPassword = password_hash ($password, PASSWORD_DEFAULT);
+
+//sql-injection preventation
+
+$stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?,?,?)");
+$stmt-> bind_param("sss", $username, $email, $hashPassword);
+//excute query
+
+if ($stmt->execute()) {
+    echo "You have registered successfully";
+}else{
+    echo "Error: " . $stmt->error;
+
+}
+$stmt->close();
+
+}
+$conn->close();
+
 ?>
